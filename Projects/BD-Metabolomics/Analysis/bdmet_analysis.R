d59b5704f41d4bd44d81ca67202256a7c42ff96a
Amber Roguski <arogusk2@ed.ac.uk>
2024-03-05 18:17:16 +0000

    updated with vars

--- Projects/BD-Metabolomics/Analysis/bdmet_analysis.R
+++ Projects/BD-Metabolomics/Analysis/bdmet_analysis.R
@@ -8,52 +8,113 @@ library(season)
 library(ggplot2)
 
 # load participant dataframe for analysis
-load(file="C:\\Users\\arogusk2\\OneDrive - University of Edinburgh\\HELIOS-BD\\Side Projects\\BioBank Project\\BD-Metabolomics data\\analysis_df.Rda")
+load(file="C:\\Users\\arogusk2\\OneDrive - University of Edinburgh\\HELIOS-BD\\Side Projects\\BioBank Project\\BD-Metabolomics data\\Metabolite Analyses\\analysis_met.Rda")
 
-# calculate mean value for each time point
-alanine_df <- analysis_df %>%
+# ANALYSE: AMINO ACIDS ####
+#amino acid df
+aa_df <- data.frame(as.numeric(NA))
+#Schizophrenia, schizotypal and delusional disorders
+aa_df$aa <- as.numeric(NA)
+aa_df$group <- as.numeric(NA)
+aa_df$month <- as.numeric(NA)
+aa_df$mean <- as.numeric(NA)
+aa_df$sd <- as.numeric(NA)
+aa_df <- aa_df[-c(1)]
+
+#alanine ####
+alanine_df <- analysis_met %>%
   group_by(assess_month, Group) %>%
-  summarise(mean_alanine = mean(alanine, na.rm=TRUE), SD_alanine = sd(alanine, na.rm=TRUE), N = length(alanine))
+  summarise(mean = mean(alanine.x, na.rm=TRUE), SD = sd(alanine.x, na.rm=TRUE), N = length(alanine.x))
+#add new column to both with study group
+alanine_df <- alanine_df %>%
+  mutate(metabolite = 'alanine')
 
-# first plot data --> choose one metabolite to get started with. VitD!
-ggplot(acetate_df, aes(x=assess_month, y=mean_acetate, color=Group)) + 
-  geom_point()
+#creatinine ####
+creatinine_df <- analysis_met %>%
+  group_by(assess_month, Group) %>%
+  summarise(mean = mean(creatinine.x, na.rm=TRUE), SD = sd(creatinine.x, na.rm=TRUE), N = length(creatinine.x))
+#add new column to both with study group
+creatinine_df <- creatinine_df %>%
+  mutate(metabolite = 'creatinine')
 
-don <- xts(x = analysis_df$vit_d, order.by = analysis_df$assess_date)
+#glutamine ####
+glutamine_df <- analysis_met %>%
+  group_by(assess_month, Group) %>%
+  summarise(mean = mean(glutamine, na.rm=TRUE), SD = sd(glutamine, na.rm=TRUE), N = length(glutamine))
+#add new column to both with study group
+glutamine_df <- glutamine_df %>%
+  mutate(metabolite = 'glutamine')
 
-ggplot(analysis_df, aes(x = acetate, y = assess_month, colour=Group)) +
-  xlab("Date") +
-  ylab("acetate level") +
-  stat_smooth(method = 'nls', formula = 'y~a*exp(b*x)',
-              method.args = list(start=c(a=0.1646, b=9.5e-8)), se=FALSE) +
-  stat_smooth(color = 1, method = 'nls', formula = 'y~a*exp(b*x)',
-              method.args = list(start=c(a=0.1646, b=9.5e-8)), se=FALSE) #+
-  #geom_point(size=4, pch=21,color = "black", stroke=1.5, aes(fill=Group))
+#glycine ####
+glycine_df <- analysis_met %>%
+  group_by(assess_month, Group) %>%
+  summarise(mean = mean(glycine, na.rm=TRUE), SD = sd(glycine, na.rm=TRUE), N = length(glycine))
+#add new column to both with study group
+glycine_df <- glycine_df %>%
+  mutate(metabolite = 'glycine')
 
-analysis_df %>%
-  ggplot( aes(x=assess_month, y=acetate, group=Group, color=Group)) +
-  geom_line()
+#histidine ####
+histidine_df <- analysis_met %>%
+  group_by(assess_month, Group) %>%
+  summarise(mean = mean(histidine, na.rm=TRUE), SD = sd(histidine, na.rm=TRUE), N = length(histidine))
+#add new column to both with study group
+histidine_df <- histidine_df %>%
+  mutate(metabolite = 'histidine')
 
-#plot
-p <- ggplot(analysis_df, aes(x=assess_month, y=alanine, colour=Group)) +
-#  geom_point() +
-  geom_smooth()+
-  scale_x_continuous(labels=as.character(analysis_df$assess_month),breaks=analysis_df$assess_month)
+#isoleucine ####
+isoleucine_df <- analysis_met %>%
+  group_by(assess_month, Group) %>%
+  summarise(mean = mean(isoleucine, na.rm=TRUE), SD = sd(isoleucine, na.rm=TRUE), N = length(isoleucine))
+#add new column to both with study group
+isoleucine_df <- isoleucine_df %>%
+  mutate(metabolite = 'isoleucine')
 
-p + scale_x_continuous(labels=c("1" = "January", "2" = "February", "3" = "March", "4" = "April", "5" = "May", "6" = "June", "7" = "July", "8" = "August", "9"="September",
-                              "10"= "October", "11"="November", "12"="December"))
+#leucine ####
+leucine_df <- analysis_met %>%
+  group_by(assess_month, Group) %>%
+  summarise(mean = mean(leucine, na.rm=TRUE), SD = sd(leucine, na.rm=TRUE), N = length(leucine))
+#add new column to both with study group
+leucine_df <- leucine_df %>%
+  mutate(metabolite = 'leucine')
 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  scale_x_continuous("assess_month", labels = as.character(analysis_df$assess_month), breaks = analysis_df$assess_month)
+#phenylalanine ####
+phenylalanine_df <- analysis_met %>%
+    group_by(assess_month, Group) %>%
+  summarise(mean = mean(phenylalanine, na.rm=TRUE), SD = sd(phenylalanine, na.rm=TRUE), N = length(phenylalanine))
+#add new column to both with study group
+phenylalanine_df <- phenylalanine_df %>%
+  mutate(metabolite = 'phenylalanine')
 
-p + scale_x_discrete(labels=c("1" = "January", "2" = "February", "3" = "March", "4" = "April", "5" = "May", "6" = "June", "7" = "July", "8" = "August", "9"="September",
-                              "10"= "October", "11"="November", "12"="December"))
+#tyrosine ####
+tyrosine_df <- analysis_met %>%
+  group_by(assess_month, Group) %>%
+  summarise(mean = mean(tyrosine, na.rm=TRUE), SD = sd(tyrosine, na.rm=TRUE), N = length(tyrosine))
+#add new column to both with study group
+tyrosine_df <- tyrosine_df %>%
+  mutate(metabolite = 'tyrosine')
 
-#plot
-ggplot(acetate_df, aes(x=assess_month, y=mean_acetate, colour=Group)) +
-  geom_point() +
-  geom_smooth()
+#join into 1 aa_df - visualise without creatinine
+aa <- rbind(alanine_df, glutamine_df, glycine_df, histidine_df, isoleucine_df, leucine_df, phenylalanine_df, tyrosine_df)
 
+#line plot only controls
+ggplot(tyrosine_df, aes(assess_month, mean))+
+  geom_line(data=tyrosine_df[tyrosine_df$Group=="control", ])+
+  ggtitle("tyrosine Annual Profile")+
+  geom_point()+
+  geom_errorbar(aes(ymin=mean-SD, ymax=mean+SD), width=.2,
+                position=position_dodge(0.05))+
+  scale_x_continuous("assess_month", labels = as.character(creatinine_df$assess_month), breaks = creatinine_df$assess_month)
 
-ggplot(subset(analysis_df,Group %in% c("bipolar"))) + 
-         geom_boxplot(aes(x=assess_month, y=alanine))
+#line plot both groups  
+ggplot(tyrosine_df, aes(assess_month, mean, color=Group))+
+  geom_line()+ggtitle("tyrosine Annual Profile")+
+  geom_point()+
+  geom_errorbar(aes(ymin=mean-SD, ymax=mean+SD), width=.2,
+                position=position_dodge(0.05))+
+  scale_x_continuous("assess_month", labels = as.character(creatinine_df$assess_month), breaks = creatinine_df$assess_month)
+
+#box plot all participants
+ggplot(analysis_met, aes(x=factor(assess_month), y=tyrosine))+
+  geom_boxplot()+is 
+  ggtitle("Tyrosine Annual Profile")+
+  scale_x_continuous("assess_month", labels = as.character(creatinine_df$assess_month), breaks = creatinine_df$assess_month)
